424819006f110a55a19348be7ad4f9de727a8ac8
Pieter-Jan <pieter-jan.robrecht@cronos.be>
2020-05-22 16:11:43 +0200

    Initial spring boot hello world app

---
+++ devs4everyone/src/main/java/be/dev4everyone/devs4everyone/Devs4everyoneApplication.java
@@ -0,0 +1,13 @@
+package be.dev4everyone.devs4everyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devs4everyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devs4everyoneApplication.class, args);
+	}
+
+}
