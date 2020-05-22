d1d31f52d661b7f89a63304a29852864bc4b5fcb
Pieter-JanRobrecht <32096923+Pieter-JanRobrecht@users.noreply.github.com>
2020-05-22 16:50:11 +0200

    Initial procfile (#1)
    * Initial procfile
    
    * Adding app.json
    
    * Restructuring repo
    
    Co-authored-by: Pieter-Jan <pieter-jan.robrecht@cronos.be>

---
+++ src/main/java/be/dev4everyone/application/Devs4everyoneApplication.java
@@ -0,0 +1,13 @@
+package be.dev4everyone.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devs4everyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devs4everyoneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/be/dev4everyone/application/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package be.dev4everyone.application.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world");
+    }
+}
