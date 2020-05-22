c706cee6c550c1bc6304ab16019e57e3ec7b5dde
Pieter-Jan <pieter-jan.robrecht@cronos.be>
2020-05-22 16:44:03 +0200

    Restructuring repo

---
+++ src/test/java/be/dev4everyone/application/Devs4everyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dev4everyone.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devs4everyoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
